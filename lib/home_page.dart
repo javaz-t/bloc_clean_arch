5573a5ec2ddcfe44ad499195864354f818eb1684
javaz-t <147338963+javaz-t@users.noreply.github.com>
2025-09-05 02:15:04 +0530

    Counter in single screen completed with cubit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,47 +1,51 @@
 import 'package:flutter/material.dart';
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_bloc_clean/counter_cubit.dart';
 
-
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key, required this.title});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final counterCubit = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('You have pushed the button this many times:'),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),)
-    );
+       appBar: AppBar(
+         backgroundColor: Theme
+             .of(context)
+             .colorScheme
+             .inversePrimary,
+         title: Text(title),
+       ),
+       body: Center(
+         child: BlocBuilder<CounterCubit, int>(
+           bloc: counterCubit, //IMP
+           builder: (context, state) {
+             return Text(
+               state.toString(),
+             );
+           },
+         ),
+       ),
+       floatingActionButton: Column(
+         mainAxisAlignment: MainAxisAlignment.end,
+         children: [
+           FloatingActionButton(
+             onPressed: () {
+               counterCubit.onIncrement();
+             },
+             tooltip: 'Increment',
+             child: const Icon(Icons.add),),
+           SizedBox(height: 10,),
+           FloatingActionButton(
+             onPressed: () {
+               counterCubit.onDecrement();
+             },
+             tooltip: 'Decriment',
+             child: const Icon(Icons.minimize),),
+         ],
+       )
+            );
   }
 }
